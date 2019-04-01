9d4112625516e577a1cd60145b6708cd1e409035
Crystal Munson <47578571+rebellekat@users.noreply.github.com>
2019-04-01 14:15:51 -0700

    fixed carousel images -
    fixed carousel images, worked on subscribe form-couldn't get subscribe button to work-right now it closes modal.
    added custom font

--- midterm/scripts/main.js
+++ midterm/scripts/main.js
@@ -13,7 +13,8 @@ $(document).ready(function(){
     });
 
     $('.modal-popper').click(function(){
-        $('.modal').modal( {backdrop: 'static'} );
+        $('.modal').modal( {backdrop: 'static'} 
+            );
     });
 
-}); 
+    });

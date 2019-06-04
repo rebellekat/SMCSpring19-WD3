38152db10026a05ed12a8f0d22cea54be5083980
Crystal Munson <47578571+rebellekat@users.noreply.github.com>
2019-06-03 23:53:26 -0700

    final project-MadDog Sails
    this final project was a frusterating experience...a lot like sailing in no wind.

--- maddog/js/myCustomScripts.js
+++ maddog/js/myCustomScripts.js
@@ -1,13 +1,12 @@
-//newsletter signup and modal
+//modal for newsletter, contact, and order placement
 $(document).ready(function(){
  
   $("#myBtn").click(function(){
     $(".newsletter_form").modal({show: true});
   });
+
   
- 
 //shopping cart
-function myFunction(){
 	document.getElementById("cartUpdate")
             $(".add-to-cart").click(function(){
                 $('.cartUpdate').html(function(i, val) {return val*1+1 });
@@ -15,13 +14,27 @@ function myFunction(){
 
 //2nd attemt at shopping cart
  var currentItems = 0;
- $(document).ready(function(){
  $(".add-to-cart").click(function(){
            currentItems++;
            $(".cartUpdate").text(currentItems);
        });
-});
 
-
-
-		
+//checkout form validation
+ (function() {
+        'use strict';
+        window.addEventListener('load', function() {
+          var forms = document.getElementsByClassName('needs-validation');
+          // Loop over them and prevent submission
+          var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+              if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+              }
+              form.classList.add('was-validated');
+            }, false);
+          });
+        }, false);
+      })();
+  });
+      
